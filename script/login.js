922bc7fb11e718e44a15af643a1b2e57ecd4ddd9
Maliha-Akter <maliha.miti.0276@gmail.com>
2026-03-08 16:02:41 +0600

    navbar part completed

---
+++ script/login.js
@@ -0,0 +1,19 @@
+document.getElementById("login-btn").addEventListener("click",function(){
+    //1. getting the input name and pass first
+    const nameInput = document.getElementById("name-input");
+    const nameInputShow = nameInput.value;
+    console.log(nameInputShow);
+
+    const passInput = document.getElementById("password-input");
+    const passInputShow = passInput.value;
+    console.log(passInputShow);
+
+    // 2. matching the input 
+    if(nameInputShow === 'admin' && passInputShow === "admin123"){
+        window.location.assign("./home.html");
+    }
+    else{
+        // window.location.replace("../html/home.html");
+        
+    }
+})
