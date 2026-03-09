0afa9e560cf0cbfbfb44a5a1c9ab0ba603eeaa2e
Maliha-Akter <maliha.miti.0276@gmail.com>
2026-03-09 21:49:04 +0600

    extra file and folder removed

--- js.js
+++
@@ -1,5 +0,0 @@
-const numbers = [1, 2, 3];
-        const newNumbers = [...numbers]; // it will copy all element 
-        console.log(newNumbers); // [1, 2, 3]
-        numbers .push (8);
-        console.log(numbers)
